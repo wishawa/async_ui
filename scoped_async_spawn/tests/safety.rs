2295530adfe61f78ac6c91d316817c024aaca0fa
Wisha Wa <wisha.wa@yandex.com>
2022-08-07 18:39:48 +0000

    scoped_async_spawn test

---
+++ scoped_async_spawn/tests/safety.rs
@@ -0,0 +1,13 @@
+use pin_utils::pin_mut;
+use scoped_async_spawn::SpawnGuard;
+
+#[test]
+fn test_lifetime() {
+    // let string = String::from("hello");
+    // let fut1 = async {
+    //     println!("{string}");
+    // };
+    // let guard = SpawnGuard::new();
+    // pin_mut!(guard);
+    // let _fut1 = guard.convert_future(fut1);
+}
