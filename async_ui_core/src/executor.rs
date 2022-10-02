cb34ac6b16df0aa869dbf27540180addf8cec11a
Wisha Wa <wisha.wa@yandex.com>
2022-10-02 20:48:18 +0000

    remove unneeded crate

--- async_ui_core/src/executor.rs
+++ async_ui_core/src/executor.rs
@@ -1,4 +1,4 @@
-use std::{future::Future, rc::Rc};
+use std::{future::{Future, pending}, rc::Rc};
 
 use async_executor::{LocalExecutor, Task};
 thread_local! {
@@ -13,8 +13,7 @@ pub fn spawn_local<F: Future + 'static>(fut: F) -> Task<F::Output> {
 
 pub(crate) fn get_driving_future() -> impl Future<Output = ()> + 'static {
     EXECUTOR.with(|exe| {
-        use async_ui_futures::PendForever;
         let exe_cpy = exe.clone();
-        async move { exe_cpy.run(PendForever).await }
+        async move { exe_cpy.run(pending()).await }
     })
 }
