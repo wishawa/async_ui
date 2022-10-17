1008e9a9debc919f01a3dcfc17549f7381d5bd60
Wisha Wa <wisha.wa@yandex.com>
2022-10-17 22:37:15 +0000

    remove unused test

--- async_ui_core/src/lib.rs
+++ async_ui_core/src/lib.rs
@@ -8,12 +8,3 @@ pub mod mount;
 pub mod position;
 pub mod vnode;
 pub use fragment::__private_macro_only;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
