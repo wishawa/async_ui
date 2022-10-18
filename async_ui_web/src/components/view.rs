094b2b9e2da911d0bf270c8b0334c090a248b010
Wisha Wa <wisha.wa@yandex.com>
2022-10-18 03:53:45 +0000

    web: view: allow custom tag

--- async_ui_web/src/components/view.rs
+++ async_ui_web/src/components/view.rs
@@ -2,13 +2,31 @@ use crate::{utils::class_list::ClassList, window::DOCUMENT, Fragment};
 
 use super::ElementFuture;
 
-#[derive(Default)]
 pub struct ViewProps<'c> {
     pub children: Fragment<'c>,
     pub class: Option<&'c ClassList<'c>>,
+    pub element_tag: &'c str,
 }
-pub async fn view<'c>(ViewProps { children, class }: ViewProps<'c>) {
-    let elem = DOCUMENT.with(|doc| doc.create_element("div").expect("create element failed"));
+impl<'c> Default for ViewProps<'c> {
+    fn default() -> Self {
+        Self {
+            children: Default::default(),
+            class: Default::default(),
+            element_tag: "div",
+        }
+    }
+}
+pub async fn view<'c>(
+    ViewProps {
+        children,
+        class,
+        element_tag,
+    }: ViewProps<'c>,
+) {
+    let elem = DOCUMENT.with(|doc| {
+        doc.create_element(element_tag)
+            .expect("create element failed")
+    });
     if let Some(class) = class {
         class.set_dom(elem.class_list());
     }
