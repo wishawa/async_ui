53e148193869e735c7385771341b5a04ae5a296c
Wisha Wa <wisha.wa@yandex.com>
2022-11-15 06:30:16 +0000

    gtk: view: width and height

--- async_ui_gtk/src/components/view.rs
+++ async_ui_gtk/src/components/view.rs
@@ -5,13 +5,23 @@ use crate::{
 
 use super::ElementFuture;
 use glib::Cast;
+use gtk::traits::WidgetExt;
 
 #[derive(Default)]
 pub struct ViewProps<'c> {
     pub children: Fragment<'c>,
+    pub width: Option<i32>,
+    pub height: Option<i32>,
 }
-pub async fn view<'c>(ViewProps { children }: ViewProps<'c>) {
+pub async fn view<'c>(
+    ViewProps {
+        children,
+        width,
+        height,
+    }: ViewProps<'c>,
+) {
     let b = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    b.set_size_request(width.unwrap_or(-1), height.unwrap_or(-1));
     ElementFuture::new(
         children,
         WrappedWidget {
