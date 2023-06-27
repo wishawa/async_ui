4afd3e5c69e6a89613bbf935f3ac051501e5458c
Wisha Wa <wisha.wa@yandex.com>
2023-06-27 11:15:05 +0000

    web: dynamic: fix for running multiple renders sequentially

--- async_ui_web/src/dynamic.rs
+++ async_ui_web/src/dynamic.rs
@@ -1,7 +1,7 @@
 use std::{
     cell::RefCell,
     future::{poll_fn, Future},
-    pin::pin,
+    pin::Pin,
     task::{Poll, Waker},
 };
 
@@ -13,11 +13,11 @@ enum Next<F> {
     NoChange,
 }
 
-pub struct Dynamic<F: Future> {
+pub struct Dynamic<F: Future + Unpin> {
     next: RefCell<(Waker, Next<F>)>,
 }
 
-impl<F: Future> Dynamic<F> {
+impl<F: Future + Unpin> Dynamic<F> {
     pub fn new() -> Self {
         Self {
             next: RefCell::new((dummy_waker(), Next::NoChange)),
@@ -25,21 +25,25 @@ impl<F: Future> Dynamic<F> {
     }
 
     pub async fn render(&self) {
-        let mut fut = pin!(None);
+        let mut fut = scopeguard::guard(None, |fut| {
+            if let Some(fut) = fut {
+                self.next.borrow_mut().1 = Next::Set(fut);
+            }
+        });
         poll_fn(|cx| {
             {
                 let mut next = self.next.borrow_mut();
                 match std::mem::replace(&mut next.1, Next::NoChange) {
-                    Next::Set(new_fut) => fut.set(Some(new_fut)),
-                    Next::Clear => fut.set(None),
+                    Next::Set(new_fut) => *fut = Some(new_fut),
+                    Next::Clear => *fut = None,
                     Next::NoChange => {}
                 }
                 if !next.0.will_wake(cx.waker()) {
                     next.0 = cx.waker().to_owned();
                 }
             }
-            if let Some(fut) = fut.as_mut().as_pin_mut() {
-                let _ = fut.poll(cx);
+            if let Some(fut) = fut.as_mut() {
+                let _ = Pin::new(fut).poll(cx);
             }
             Poll::Pending
         })
