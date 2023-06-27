0b1f579adab8839bc4329751648f4a28329e837e
Wisha Wa <wisha.wa@yandex.com>
2023-06-27 09:36:47 +0000

    examples/big-list: implement recycling here since it was removed from VirtualizedList

--- examples/big-list/src/app.rs
+++ examples/big-list/src/app.rs
@@ -1,17 +1,24 @@
-use async_ui_web::{
-    components::Div,
-    shortcut_traits::{ShortcutClassList, ShortcutRenderStr},
-    VirtualizedList,
-};
+use std::cell::RefCell;
+
+use async_ui_web::{components::Div, shortcut_traits::ShortcutClassList, NoChild, VirtualizedList};
 
 pub async fn app() {
     let root = Div::new();
     root.add_class(style::wrapper);
+    let divs = &RefCell::new(Vec::new());
     let list = VirtualizedList::new(
         &root.element,
         Div::new().element.into(),
         Div::new().element.into(),
-        |recycle, index| Div::new().render(index.to_string().render()),
+        |index| async move {
+            let div = divs.borrow_mut().pop().unwrap_or_else(Div::new);
+            div.set_text_content(Some(&index.to_string()));
+            let fut = div.render(NoChild);
+            scopeguard::defer!(
+                divs.borrow_mut().push(div);
+            );
+            fut.await
+        },
     );
     list.set_num_items(100000);
     root.render(list.render()).await;
