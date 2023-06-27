2973f7edc9fb2306a5adae2646cb58eed3cb2fe7
Wisha Wa <wisha.wa@yandex.com>
2023-06-27 09:52:14 +0000

    web: various improvements to virtualizedlist, also removed recycling (users can do that themself pretty easily)

--- async_ui_web/src/virtualized_list.rs
+++ async_ui_web/src/virtualized_list.rs
@@ -3,31 +3,31 @@ use std::{
     future::pending,
     hash::Hash,
     ops::{Range, RangeBounds},
+    rc::Rc,
 };
 
-use async_ui_web_core::{combinators::join, window, ContainerNodeFuture};
-use futures_lite::Future;
-use js_sys::Function;
-use wasm_bindgen::{JsValue, UnwrapThrowExt};
+use async_ui_internal_utils::reactive_cell::ReactiveCell;
+use async_ui_web_core::{combinators::join, ContainerNodeFuture};
+use futures_lite::{Future, StreamExt};
+use wasm_bindgen::{prelude::Closure, JsCast, JsValue, UnwrapThrowExt};
 use web_sys::{Element, HtmlElement, IntersectionObserver, IntersectionObserverInit};
 
-use crate::{callback_to_future::CallbackToFuture, utils::MiniScopeGuard, DynamicList};
+use crate::{utils::MiniScopeGuard, DynamicList};
 
-pub struct VirtualizedList<'c, Fut: Future + 'c, Updater: FnMut(Option<usize>, usize) -> Fut> {
+pub struct VirtualizedList<'c, Fut: Future + 'c, Updater: FnMut(usize) -> Fut> {
     list: DynamicList<'c, usize, Fut>,
     state: RefCell<State<Updater>>,
     spacers: (HtmlElement, HtmlElement),
     direction: Direction,
-    root: &'c Element,
-    refresh: CallbackToFuture<JsValue, ()>,
+    root: &'c HtmlElement,
+    signal: Rc<ReactiveCell<()>>,
+    wake_closure: Closure<dyn Fn(JsValue)>,
 }
 
 struct State<Updater> {
-    reuse_stack: Vec<usize>,
     updater: Updater,
     range: Range<usize>,
     num_items: usize,
-    callback_id: Option<i32>,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
@@ -36,29 +36,31 @@ pub enum Direction {
     Horizontal,
 }
 
-impl<'c, Fut: Future + 'c, Updater: FnMut(Option<usize>, usize) -> Fut>
-    VirtualizedList<'c, Fut, Updater>
-{
+impl<'c, Fut: Future + 'c, Updater: FnMut(usize) -> Fut> VirtualizedList<'c, Fut, Updater> {
     pub fn new(
-        root: &'c Element,
+        root: &'c HtmlElement,
         spacer_front: HtmlElement,
         spacer_back: HtmlElement,
         updater: Updater,
     ) -> Self {
         let state = State {
-            reuse_stack: Vec::new(),
             updater,
             range: 0..0,
             num_items: 0,
-            callback_id: None,
         };
+        let signal = Rc::new(ReactiveCell::new(()));
+        let signal_cloned = signal.clone();
         Self {
             list: DynamicList::new(),
             spacers: (spacer_front, spacer_back),
             state: RefCell::new(state),
             direction: Direction::Vertical,
             root,
-            refresh: CallbackToFuture::new(|_| ()),
+            signal,
+            wake_closure: Closure::new(move |_: JsValue| {
+                signal_cloned.borrow_mut();
+                async_ui_web_core::executor::run_now();
+            }),
         }
     }
     pub fn set_direction(&mut self, direction: Direction) {
@@ -77,14 +79,29 @@ impl<'c, Fut: Future + 'c, Updater: FnMut(Option<usize>, usize) -> Fut>
         let [spf_render, spb_render] = [spf, spb]
             .map(|spacer| ContainerNodeFuture::new(pending::<()>(), spacer.clone().into()));
         let guard = MiniScopeGuard(|| {
-            if let Some(callback_id) = self.state.borrow_mut().callback_id {
-                window::WINDOW.with(|w| w.clear_timeout_with_handle(callback_id));
-            }
+            self.root
+                .remove_event_listener_with_callback(
+                    "scroll",
+                    self.wake_closure.as_ref().unchecked_ref(),
+                )
+                .unwrap_throw();
         });
         join((spf_render, self.list.render(), spb_render, async {
-            let _observer = Observer::new(&self.root, &[spf, spb], self.refresh.get_function());
+            let _observer = Observer::new(
+                &self.root,
+                &[spf, spb],
+                self.wake_closure.as_ref().unchecked_ref(),
+            );
+            self.update_visible();
+            self.root
+                .add_event_listener_with_callback(
+                    "scroll",
+                    self.wake_closure.as_ref().unchecked_ref(),
+                )
+                .unwrap_throw();
+            let mut uc = self.signal.until_change();
             loop {
-                self.refresh.signal.until_change().await;
+                uc.next().await;
                 self.update_visible();
             }
         }))
@@ -102,14 +119,13 @@ impl<'c, Fut: Future + 'c, Updater: FnMut(Option<usize>, usize) -> Fut>
         let rect_b = top_bottom(&spb.get_bounding_client_rect());
         let rect_root = top_bottom(&self.root.get_bounding_client_rect());
         let root_size = rect_root.1 - rect_root.0;
-        if (rect_f.1 + root_size < rect_root.0 || state.range.start == 0)
-            && (rect_b.0 - root_size > rect_root.1 || state.range.end == state.num_items)
+        let tres_size = root_size * 1.25;
+        let pad_size = root_size * 2.0;
+        if (rect_f.1 + tres_size < rect_root.0 || state.range.start == 0)
+            && (rect_b.0 - tres_size > rect_root.1 || state.range.end == state.num_items)
         {
-            web_sys::console::log_1(&"all good".into());
-            state.callback_id = None;
             return;
         }
-        web_sys::console::log_1(&"update".into());
 
         let num_visible = state.range.len();
         let avg_size = if num_visible > 0 {
@@ -117,48 +133,36 @@ impl<'c, Fut: Future + 'c, Updater: FnMut(Option<usize>, usize) -> Fut>
         } else {
             root_size / 4.0
         };
-        let new_start = ((rect_root.0 - root_size - rect_f.0) / avg_size).max(0.0) as usize;
-        let new_end = (((rect_root.1 + root_size - rect_f.0) / avg_size).ceil() as usize)
-            .min(state.num_items);
+        let new_start = ((rect_root.0 - pad_size - rect_f.0) / avg_size).max(0.0) as usize;
+        let new_end =
+            (((rect_root.1 + pad_size - rect_f.0) / avg_size).ceil() as usize).min(state.num_items);
 
         let preferred_height_f = avg_size * new_start as f64;
         let preferred_height_b = avg_size * (state.num_items - new_end) as f64;
 
-        spf.style()
-            .set_property("height", &format!("{preferred_height_f}px"))
-            .unwrap_throw();
-        spb.style()
-            .set_property("height", &format!("{preferred_height_b}px"))
-            .unwrap_throw();
-        web_sys::console::log_1(
-            &format!("range: {new_start}..{new_end}, avg_size: {avg_size:.2}").into(),
-        );
+        {
+            spf.style()
+                .set_property("block-size", &format!("{preferred_height_f}px"))
+                .ok();
+            spb.style()
+                .set_property("block-size", &format!("{preferred_height_b}px"))
+                .ok();
+        }
         for to_remove in (state.range.start..state.range.end.min(new_start))
             .chain(state.range.start.max(new_end)..state.range.end)
         {
             self.list.remove(&to_remove);
-            state.reuse_stack.push(to_remove);
         }
         let remaining = (state.range.start >= new_start && state.range.start < new_end)
             .then_some(state.range.start);
         for to_add in new_start..new_end.min(state.range.start) {
-            let reuse = state.reuse_stack.pop();
-            let new_fut = (state.updater)(reuse, to_add);
-            self.list.insert(to_add, new_fut, remaining.as_ref());
+            self.list
+                .insert(to_add, (state.updater)(to_add), remaining.as_ref());
         }
         for to_add in new_start.max(state.range.end)..new_end {
-            let reuse = state.reuse_stack.pop();
-            let new_fut = (state.updater)(reuse, to_add);
-            self.list.insert(to_add, new_fut, None);
+            self.list.insert(to_add, (state.updater)(to_add), None);
         }
         state.range = new_start..new_end;
-        state.callback_id = Some(window::WINDOW.with(|w| {
-            w.set_timeout_with_callback_and_timeout_and_arguments_0(
-                self.refresh.get_function(),
-                100,
-            )
-            .unwrap_throw()
-        }));
     }
 }
 
@@ -167,15 +171,16 @@ struct Observer {
 }
 
 impl Observer {
-    fn new(root: &Element, spacers: &[&HtmlElement], function: &Function) -> Self {
+    fn new(root: &Element, spacers: &[&HtmlElement], wake: &js_sys::Function) -> Self {
         let observer = IntersectionObserver::new_with_options(
-            function,
+            wake,
             IntersectionObserverInit::new()
                 .root(Some(root))
                 .root_margin("100%"),
         )
         .unwrap_throw();
         spacers.iter().for_each(|sp| observer.observe(sp));
+
         Self { observer }
     }
 }
