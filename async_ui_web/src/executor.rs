b7ccb3c45963c13882f918eb57c3b08b1ce3a504
Wisha Wa <wisha.wa@yandex.com>
2023-07-13 14:33:05 +0000

    web: switch to OnceCell for executor

--- async_ui_web/src/executor.rs
+++ async_ui_web/src/executor.rs
@@ -3,26 +3,23 @@
 //! Async UI Web creates an app-wide executor
 //! Use the [get_executor] function to access the executor.
 
-use std::{cell::Cell, future::pending};
+use std::{cell::OnceCell, future::pending};
 
 use async_executor::LocalExecutor;
 use async_ui_web_core::executor::set_executor_future;
 
 thread_local! {
-    static EXECUTOR: Cell<Option<&'static LocalExecutor<'static>>> = Cell::new(None);
+    static EXECUTOR: OnceCell<&'static LocalExecutor<'static>> = OnceCell::new();
 }
 
 /// Get the executor that is driving the framework.
 /// Use this executor to spawn your own tasks if you want.
 pub fn get_executor() -> &'static LocalExecutor<'static> {
     EXECUTOR.with(|cell| {
-        if let Some(exe) = cell.get() {
+        *cell.get_or_init(|| {
+            let exe = Box::leak(Box::new(LocalExecutor::new()));
+            set_executor_future(Box::new(exe.run(pending())));
             exe
-        } else {
-            let leaked = Box::leak(Box::new(LocalExecutor::new()));
-            cell.set(Some(&*leaked));
-            set_executor_future(Box::new(leaked.run(pending())));
-            &*leaked
-        }
+        })
     })
 }
