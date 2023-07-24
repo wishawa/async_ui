5350dad4ec53f5e5cadd67bbc370ca3cda2f2ff7
Wisha Wa <wisha.wa@yandex.com>
2023-07-24 08:02:40 +0000

    rename Dynamic to DynamicSlot and enforce render only once

--- async_ui_web/src/components/dynamic_slot.rs
+++ async_ui_web/src/components/dynamic_slot.rs
@@ -1,25 +1,19 @@
 use std::{
     cell::RefCell,
     future::{poll_fn, Future},
-    pin::Pin,
+    pin::pin,
     task::{Poll, Waker},
 };
 
 use async_ui_internal_utils::dummy_waker::dummy_waker;
 
-enum Next<F> {
-    Set(F),
-    Clear,
-    NoChange,
-}
-
 /**
 A "slot" that renders a future which you can change dynamically.
 
 ```
-# use async_ui_web::{components::Dynamic, join, html::Button, prelude_traits::*};
+# use async_ui_web::{components::DynamicSlot, join, html::Button, prelude_traits::*};
 # let _ = async {
-let slot = Dynamic::new();
+let slot = DynamicSlot::new();
 let btn = Button::new();
 let mut count = 0;
 join((
@@ -38,48 +32,54 @@ join((
 # };
 ```
  */
-pub struct Dynamic<F: Future + Unpin> {
+pub struct DynamicSlot<F: Future> {
     next: RefCell<(Waker, Next<F>)>,
 }
 
-impl<F: Future + Unpin> Default for Dynamic<F> {
+enum Next<F> {
+    Set(F),
+    Clear,
+    NoChange,
+}
+
+impl<F: Future> Default for DynamicSlot<F> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<F: Future + Unpin> Dynamic<F> {
-    /// Create a new `Dynamic`, containing no future inside.
+impl<F: Future> DynamicSlot<F> {
+    /// Create a new `Dynamic`, containing no Future inside.
     pub fn new() -> Self {
         Self {
             next: RefCell::new((dummy_waker(), Next::NoChange)),
         }
     }
 
-    /// Render the future inside the `Dynamic` here.
+    /// Render the Future set inside the `Dynamic` here.
     ///
     /// The UI will update when you call
     /// [set_future][Dynamic::set_future] or [clear_future][Dynamic::clear_future].
+    ///
+    /// This async method never completes.
+    ///
+    /// This method should only ba called once. It may misbehave otherwise.
     pub async fn render(&self) {
-        let mut fut = scopeguard::guard(None, |fut| {
-            if let Some(fut) = fut {
-                self.next.borrow_mut().1 = Next::Set(fut);
-            }
-        });
+        let mut fut = pin!(None);
         poll_fn(|cx| {
             {
                 let mut next = self.next.borrow_mut();
                 match std::mem::replace(&mut next.1, Next::NoChange) {
-                    Next::Set(new_fut) => *fut = Some(new_fut),
-                    Next::Clear => *fut = None,
+                    Next::Set(new_fut) => fut.set(Some(new_fut)),
+                    Next::Clear => fut.set(None),
                     Next::NoChange => {}
                 }
                 if !next.0.will_wake(cx.waker()) {
                     next.0 = cx.waker().to_owned();
                 }
             }
-            if let Some(fut) = fut.as_mut() {
-                let _ = Pin::new(fut).poll(cx);
+            if let Some(fut) = fut.as_mut().as_pin_mut() {
+                let _ = fut.poll(cx);
             }
             Poll::Pending
         })
--- async_ui_web/src/components/mod.rs
+++ async_ui_web/src/components/mod.rs
@@ -3,7 +3,7 @@
 //! Components in this module are provided for convenience.
 
 mod dropdown;
-mod dynamic;
+mod dynamic_slot;
 
 pub use dropdown::Dropdown;
-pub use dynamic::Dynamic;
+pub use dynamic_slot::DynamicSlot;
